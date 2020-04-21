586bc26b4a41f22358e75874765d3bc6703eca77
Victor Hugo Marra Araujo <31578843+vhmarra@users.noreply.github.com>
2020-04-21 17:43:48 -0300

    Add files via upload
    scrip that generate youtube video url using google's api and run through selenium module.
    
    *ps: the selenium webdriver is for google chorme, if you want to run in another browser look for selenium documentation on webdriver for your chosen browser

---
+++ youtube_random_url_generator_final.py
@@ -0,0 +1,32 @@
+import json
+import urllib.request
+import string
+from selenium import webdriver #pip install selenium
+import time
+
+
+count = 1
+API_KEY = 'YOUR KEY' #put your api key here
+random = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(3))
+urlData = "https://www.googleapis.com/youtube/v3/search?key={}&maxResults={}&part=snippet&type=video&q={}".format(API_KEY,count,random)
+webURL = urllib.request.urlopen(urlData)
+data = webURL.read()
+encoding = webURL.info().get_content_charset('utf-8')
+results = json.loads(data.decode(encoding))
+
+for data in results['items']:
+     videoId = (data['id']['videoId'])
+
+last_part_url = (data['id']['videoId']) #11 digits base64 video ID
+    
+class bot_video_youtube():
+    def __init__(self):
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument("--incognito")
+        self.driver = webdriver.Chrome(options=self.options)
+        url = ("https://www.youtube.com/watch?v="+str(last_part_url))
+        print("video url: ",url)
+        print("video base64 id: ",last_part_url)
+        self.driver.get(url)
+ 
+bot = bot_video_youtube()
